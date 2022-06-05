b831853d8f4c92047d7217c24c13a769cf2f1190
Hackerprod <Hackerprodlive@gmail.com>
2022-06-05 00:06:41 -0400

    Update

--- SKYNET.INISerializer Example/Settings.cs
+++ SKYNET.INISerializer Example/Settings.cs
@@ -2,10 +2,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SKYNET
 {
--- SKYNET.INISerializer/INISerializer.cs
+++ SKYNET.INISerializer/INISerializer.cs
@@ -1,14 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
-using SKYNET.INI.Attributes;
 
 namespace SKYNET.INI
 {
